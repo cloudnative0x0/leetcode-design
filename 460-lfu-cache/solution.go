ad703b9412bf9570a636785a8e25790b3f0c6c9f
Herman <hedgevector@icloud.com>
2026-03-16 23:14:49 +0300

    SLVD: solution's pattern added.

---
+++ 460-lfu-cache/solution.go
@@ -0,0 +1,22 @@
+package _460_lfu_cache
+
+type LFUCache struct {
+}
+
+func Constructor(capacity int) LFUCache {
+	return LFUCache{}
+}
+
+func (this *LFUCache) Get(key int) int {
+	return 0
+}
+
+func (this *LFUCache) Put(key int, value int) {
+}
+
+/**
+ * Your LFUCache object will be instantiated and called as such:
+ * obj := Constructor(capacity);
+ * param_1 := obj.Get(key);
+ * obj.Put(key,value);
+ */
